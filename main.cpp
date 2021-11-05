32fa583c409dd79fb4e8ba1cbd90b8a4786750f3
windowdong11 <windowdong11@naver.com>
2021-11-05 13:30:50 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 #include "test/TestCase.h"
 #endif
 using namespace std;
+void solve(){
+  // code here
+}
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
-  //testExample();
+  #ifndef ONLINE_JUDGE
+  testExample(); // runTests(solve, {"tcfile1.txt", "tcfile2.txt"})
+  #else
+  solve();
+  #endif
   return 0;
 }
