bc9b201493dbd6a2b42facc79e294aa5684167c3
windowdong11 <windowdong11@naver.com>
2021-11-05 17:30:48 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,36 @@
 #include "test/TestCase.h"
 #endif
 using namespace std;
+
 void solve(){
-  // code here
 }
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   #ifndef ONLINE_JUDGE
-  runTests(solve, {}); // runTests(solve, {"tcfile1.txt", "tcfile2.txt"})
+  int tcCnt = 7;// set number of test cases
+  runTests(solve, [&]() -> vector<string> { // generate testcase file name, ex) "tc1.txt"
+    vector<string> f;
+    for(int i = 1; i <= tcCnt; ++i)
+      f.push_back(
+        "tc"
+        + [](int num) -> string // int to string
+          {
+            char c[] = "0123456789";
+            string s;
+            while (num)
+            {
+              s += c[num % 10];
+              num /= 10;
+            }
+            reverse(s.begin(), s.end());
+            return s;
+          }(i)
+        + ".txt");
+    return f;
+  }());
   #else
   solve();
   #endif
